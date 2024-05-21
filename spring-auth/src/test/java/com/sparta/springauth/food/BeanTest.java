3ab123945191dfed373ef89223c10a3b4fd00b7f
dami0806 <dami0804@naver.com>
2024-05-21 13:33:23 +0900

    week3: 같은타입의 빈 등록

---
+++ spring-auth/src/test/java/com/sparta/springauth/food/BeanTest.java
@@ -0,0 +1,20 @@
+package com.sparta.springauth.food;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeanTest {
+    @Autowired
+    @Qualifier("pizza")
+    Food food;
+
+    @Test
+    @DisplayName("Primary와 Quilifier 우선순위 확인")
+    public void test() {
+        food.eat();
+    }
+}
